add63c7eb669c755e8b93df3d1d6ab9a71c909de
Jerry Lerman <lerman@runbox.com>
2018-10-23 20:42:51 -0400

    Added projects page

--- server.js
+++ server.js
@@ -53,8 +53,14 @@ app.get('/',(req, res) => {
 app.get('/about',(req, res) => {
   //res.send('About Page');
   res.render('about.hbs', {
-    pageTitle: 'About Page',
+    pageTitle: 'About Page'
     //currentYear: new Date().getFullYear()
+  });
+});
+
+app.get('/projects',(req, res) => {
+  res.render('projects.hbs', {
+    pageTitle: 'This is the projects page'
   });
 });
 
